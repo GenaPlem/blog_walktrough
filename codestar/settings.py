e324bf8e908e450fc877c434f17e4950d629fd2b
GenaPlem <shainkill1337@gmail.com>
2023-10-24 11:08:12 +0100

    Update allowed hosts link

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gg-django-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['gg-django-blog-39f1ceb9836d.herokuapp.com', 'localhost']
 
 
 # Application definition
