e750ddd365fe4a5541c9bf8f4437e834eb93cb09
GenaPlem <shainkill1337@gmail.com>
2023-10-24 21:30:11 +0100

    Add comments CRUD sistem

--- codestar/settings.py
+++ codestar/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
+    "crispy_bootstrap4",
     'blog',
 ]
 
@@ -58,6 +60,10 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
